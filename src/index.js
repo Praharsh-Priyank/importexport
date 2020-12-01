be213c10d904e6c907168b21f61f1561ebfbe788
Praharsh-Priyank <52607687+Praharsh-Priyank@users.noreply.github.com>
2020-12-01 05:30:06 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import pi from "./math.js";
+ReactDOM.render(
+  <ul>
+    <li>{pi}</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>,
+  document.getElementById("root")
+);
